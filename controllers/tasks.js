352ea1bb7925696679ddb04b0fe5e2129deeba4a
MohammadRokib <rokibk548@gmail.com>
2023-10-16 19:15:37 +0600

    added database schema

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,35 +1,45 @@
+const Task = require('../models/Task.js');
+
 //--  Get all task  --\\
-const getTask = (req, res) => {
-    res.status(200).json({ success:true, msg:`Getting all tasks` });
+const getTask = async (req, res) => {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
 };
 
 //--  Create new task  --\\
-const createTask = (req, res) => {
-    const { name } = req.body;
-
-    res.status(200).json({ success:true, msg:`Creating new task: ${name}` });
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
 };
 
 //--  Get single task  --\\
-const getSingleTask = (req, res) => {
+const getSingleTask = async (req, res) => {
     const { id:taskID } = req.params;
-
-    res.status(200).json({ success:true, msg:`Getting task ${taskID} `});
+    const task = await Task.findOne({ _id:taskID });
+    if (!task) {
+        return res.status(404).json({ success:false, msg:`No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
 };
 
 //--  Update task  --\\
-const updateTask = (req, res) => {
+const updateTask = async (req, res) => {
     const { id:taskID } = req.params;
-    const { name } = req.body;
-
-    res.status(200).json({ success:true, msg:`Updating task ${taskID} with ${name} `});
+    const task = await Task.findOneAndUpdate({ _id:taskID}, req.body, {
+        new: true,
+        runValidators: true,
+    });
+    res.status(200).json({ task });
 };
 
 //--  Delete task  --\\
-const deleteTask = (req, res) => {
+const deleteTask = async (req, res) => {
     const { id:taskID } = req.params;
-
-    res.status(200).json({ success:true, msg:`Deleting task ${taskID} `});
+    const task = await Task.findOneAndDelete({ _id:taskID });
+    if (!task) {
+        return res.status(404).json({ success:false, msg:`No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task});
 };
 
 module.exports = {
